fdbd7187b05da825552221518c6f80fbde5c14e4
LuckyGonosz <58446607+LuckyGonosz@users.noreply.github.com>
2021-12-17 16:07:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/ash
-echo "NEW ENTRYPOINT"
+
 # Getting the lastest version of the infra we need to deploy and running it.
-git clone https://github.com/devops-moonwalkers/gcp-infra.git
-cd gcp-infra/
-cp /app/backend.tf /app/gcp-infra/backend.tf -f
-cp /app/terraform.tfvars /app/gcp-infra/terraform.tfvars -f
+git clone https://github.com/NicolasHostert/ycit021-terraform.git
+cd ycit021-terraform/
+cp /app/backend.tf /app/ycit021-terraform/backend.tf -f
+cp /app/terraform.tfvars /app/ycit021-terraform/terraform.tfvars -f
 
 terraform init
 terraform apply -auto-approve
